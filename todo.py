8c61fca064a34d6998e11cb13f9cd5754c7032b6
JoshuaTheEpic1 <10877384@uvu.edu>
2024-09-05 11:07:19 -0600

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,2 +1,10 @@
 def  add_task(task, todo_list):
   todo_list.append(task)
+
+todo_list = [(1,'First Task'),(2,"Second Task")]
+def search(name):
+  for item in todo_list:
+    if item[1] == name:
+      return item[0]
+    
+print(search("Second Task"))
